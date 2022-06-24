0b496fe45f0ff21d4ec6932a42bd54a1cb5e9c97
Ian Luan <ian.oliveira@lais.huol.ufrn.br>
2022-06-24 11:36:45 -0300

    feat: add release year in search

--- quickboxd-extension/content/content.js
+++ quickboxd-extension/content/content.js
@@ -8,13 +8,14 @@ const injectQuickboxd = async () => {
   try {
     const paths = window.location.pathname.split("/");
     const netflixResponse = await (await fetch(netflixApi + paths[paths.length-1])).json();
-    const tmdbResponse = await (await fetch(tmdbApi + netflixResponse.video.title)).json();
+    const tmdbResponse = await (
+      await fetch(tmdbApi + netflixResponse.video.title + "&primary_release_year=" + netflixResponse.video.year)
+    ).json();
     
     if (tmdbResponse.total_results > 0) {
       movieData = await (await fetch(quickApi + "?id=" + tmdbResponse.results[0].id)).json();
       
       chrome.storage.sync.set({'quickboxd': movieData}, function() {
-        console.log(movieData)
         console.log('Quickboxd is done!');
       });
     }
@@ -33,6 +34,4 @@ chrome.runtime.onMessage.addListener(function(request) {
 
 if(window.location.pathname && window.location.pathname.indexOf("/watch/") > -1) {
   injectQuickboxd();
-} else {
-  console.log("rejected")
 }
